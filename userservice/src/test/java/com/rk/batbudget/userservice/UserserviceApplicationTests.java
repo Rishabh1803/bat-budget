6044ae279d048793101cd8a9f07ac2601cea4991
Rishabh1803 <rishabhkhare18@gmail.com>
2025-05-04 04:04:08 +0530

    https://github.com/Rishabh1803/bat-budget/issues/30 Setup initial module

---
+++ userservice/src/test/java/com/rk/batbudget/userservice/UserserviceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.rk.batbudget.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // TODO document why this method is empty
+    }
+
+}
