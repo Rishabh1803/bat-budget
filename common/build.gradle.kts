e230dbfc1f2077ff941ee3ab0de069dd7c23f25f
Rishabh1803 <rishabhkhare18@gmail.com>
2025-05-11 03:02:12 +0530

    https://github.com/Rishabh1803/bat-budget/issues/31 Remove main class of common module

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,11 +1,7 @@
 plugins {
-    java
-    id("org.springframework.boot") version "3.4.5"
+    id("java-library")
     id("io.spring.dependency-management") version "1.1.7"
 }
-
-group = "com.rk.batbudget"
-version = "0.0.1-SNAPSHOT"
 
 java {
     toolchain {
@@ -24,15 +20,13 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("org.postgresql:postgresql")
     annotationProcessor("org.projectlombok:lombok")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
-    useJUnitPlatform()
+    useJUnit()
 }
